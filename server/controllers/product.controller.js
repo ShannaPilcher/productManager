207bea0546ad0abaceb06eceaba441737a1f261b
Shanna Pilcher <shannampilcher@gmail.com>
2021-03-25 17:12:37 -0600

    Some files were missed in original commit

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,8 +1,14 @@
 const Product = require('../models/product.models');
-module.exports.findAllProducts = (req, res) => {
-    Product.find()
-        .then(allProducts => res.json({products: allProducts}))
+module.exports.getAllProducts = (req, res) => {
+    Product.find({})
+        .then(products => res.json(products))
         .catch(err => res.json({message: "Something went wrong with Product.find", error: err}))
+}
+
+module.exports.getProduct = (req, res) => {
+    Product.findOne({_id:req.params.id})
+        .then(product => res.json(product))
+        .catch(err => res.json(err))
 }
 
 module.exports.createProduct = (req, res) => {
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,6 +1,7 @@
 const ProductController = require('../controllers/product.controller');
 
 module.exports = app => {
-    app.get('/api/products', ProductController.findAllProducts)
+    app.get('/api/products', ProductController.getAllProducts)
+    app.get('/api/products/:id', ProductController.getProduct)
     app.post('/api/products', ProductController.createProduct)
 }
